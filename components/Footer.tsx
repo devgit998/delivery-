421ac2c1f4d7294d69fb584026b6a76a78d12fe5
devgit <khuzzkhuzz5@gmail.com>
2026-02-07 16:48:29 +0100

    update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,11 +3,14 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="bg-amber-400 text-center p-4 mt-8 text-stone-950">
-      speedX has affiliation with FEDEX, UPS, DHL, USPS, and other major
-      carriers. We provide a seamless shipping experience by integrating with
-      these carriers to offer competitive rates and reliable delivery options.
-      Whether you&apos;re shipping domestically or internationally, speedX
-      ensures that your packages are handled with care and delivered on time.
+      <span>
+        speedX has affiliation with FEDEX, UPS, DHL, USPS, and other major
+        carriers. We provide a seamless shipping experience by integrating with
+        these carriers to offer competitive rates and reliable delivery options.
+        Whether you&apos;re shipping domestically or internationally, speedX
+        ensures that your packages are handled with care and delivered on time.
+      </span>
+      <p>&copy; 2026 Your Company. All rights reserved.</p>
     </div>
   );
 };
